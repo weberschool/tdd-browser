a20b46771d4d0e9fdc953d3d7ec2719c138389b7
Jonata Weber <jonataa@gmail.com>
2017-12-16 16:16:53 -0300

    initial commit

---
+++ tests/fizzbuzz_test.js
@@ -0,0 +1,9 @@
+var expect = chai.expect;
+
+describe('Fizzbuzz', function() {
+    describe('fizz', function() {
+        it('test', function() {
+            expect(true).to.equal(true);
+        });
+    });
+});
